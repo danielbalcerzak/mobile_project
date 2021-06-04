23bc205481e91a97acfa4a552a35e3b7f073d806
Daniel Balcerzak <danielbalcerzak@gmail.com>
2021-06-04 21:54:10 +0200

    starting linear process in queue in each mobile operators

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import depot as dp
 from random import randint as rint
 from random import choice
 
-NUM_OF_ACTIONS = 300
+NUM_OF_ACTIONS = 100
 MIN_KB = 1
 MAX_KB = 6
 
@@ -20,6 +20,12 @@ def main():
                 user.send_mms(choice(users_list), rint(MIN_KB, MAX_KB))
             elif action == 3:
                 user.calling(choice(users_list))
+
+    for operator in operator_list:
+        item_list = operator.get_list_of_items()
+        operator.starting_process_in_queue(item_list)
+
+    users_list[0].get_info()
 
 
 if __name__ == '__main__':
