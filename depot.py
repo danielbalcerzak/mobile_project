c86f2babaf9cd3585beff479ff7069b73c54e2e0
deltaB <daniel.balcerzak@gmail.com>
2021-06-09 22:46:19 +0200

    clean some things / statement

--- depot.py
+++ depot.py
@@ -2,7 +2,6 @@ import mobile_operator
 import client
 from random import randint as rint
 from random import choice
-from os.path import isfile
 
 
 # client variables
@@ -26,6 +25,7 @@ OPERATOR_DICT = {"era": "+10",
                  "redbull": "+14"}
 USERS = []
 
+
 def start_operators_action(operator_list):
     for operator in operator_list:
         item_list = operator.get_list_of_items()
@@ -43,6 +43,7 @@ def start_send_and_call(users_list):
                 user.send_mms(choice(users_list), rint(MIN_KB, MAX_KB))
             elif action == 3:
                 user.calling(choice(users_list))
+
 
 def making_operator_list():
     return [mobile_operator.MobileOperator(operator) for operator in OPERATOR_DICT]
@@ -79,46 +80,3 @@ def get_exception(default, exception, function, *args):
         return function(*args)
     except exception:
         return default
-
-
-def save_data_delivered(item, operator):
-    if operator.name == item.from_who.operator.name:
-        if not isfile(f'operator_{item.from_who.operator.name}_bill.csv'):
-            with open(f'operator_{item.from_who.operator.name}_bill.csv', 'w', encoding="utf8") as outfile:
-                description_list = ['sender_operator',
-                                    'sender_prefix',
-                                    'sender_phonenumber',
-                                    'recipient_operator',
-                                    'recipient_prefix',
-                                    'recipient_phonenumber',
-                                    'msg_type',
-                                    'msg_text',
-                                    'msg_size',
-                                    'send_date',
-                                    'send_time']
-                for element in description_list:
-                    if element is description_list[len(description_list)-1]:
-                        outfile.write(element + '\n')
-                    else:
-                        outfile.write(element)
-                        outfile.write(",")
-
-        with open(f'operator_{item.from_who.operator.name}_bill.csv', 'a', encoding="utf8") as csvfile:
-            csvfile_info_list = [item.from_who.operator.name,
-                                 item.from_who.operator.prefix,
-                                 item.from_who.nr_tel,
-                                 item.msg_recipient.operator.name,
-                                 item.msg_recipient.operator.prefix,
-                                 item.msg_recipient.nr_tel,
-                                 item.msg_type,
-                                 item.msg_text,
-                                 item.msg_size,
-                                 item.msg_send_date,
-                                 item.msg_send_time]
-            for info in csvfile_info_list:
-                if info is csvfile_info_list[len(csvfile_info_list) - 1]:
-                    csvfile.write(str(info))
-                    csvfile.write('\n')
-                else:
-                    csvfile.write(str(info))
-                    csvfile.write(",")
