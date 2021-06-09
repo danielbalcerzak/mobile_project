c4cb2682c67e124b0c9bf0566e91b258dc20dc21
deltaB <daniel.balcerzak@gmail.com>
2021-06-09 22:46:40 +0200

    clean some things / statement

--- client.py
+++ client.py
@@ -1,6 +1,8 @@
+import os
 from random import randint as rint
 from message import Sms, Mms, Call
 import depot as dp
+from os.path import isfile, isdir
 
 
 class Client:
@@ -10,49 +12,49 @@ class Client:
         self.nr_tel = self.operator.prefix + self.short_nr_tel
         self.max_sms = max_sms
         self.max_mms_memo = max_mms
-        self.sms_income_history = []
         self.sms_received_history = []
-        self.mms_income_history = []
+        self.sms_send_history = []
         self.mms_received_history = []
-        self.call_income_history = []
+        self.mms_send_history = []
         self.call_received_history = []
+        self.call_send_history = []
 
     def get_info(self):
         print(f"Phone number: {self.nr_tel}")
         print(f"Operator: {self.operator.name}")
         print(f"Prefix: {self.operator.prefix}")
         print(f"Short phone number: {self.short_nr_tel}")
-        print(f"Memory for sms: {len(self.sms_received_history) + len(self.sms_income_history)} / {self.max_sms}")
-        print(f"\tsms sent: {len(self.sms_received_history)}")
-        print(f"\tsms received: {len(self.sms_income_history)}")
-        print(f"Memory for mms: {len(self.mms_received_history) + len(self.mms_income_history)} / {self.max_mms_memo}")
-        print(f"\tmms sent: {len(self.mms_received_history)}")
-        print(f"\tmms received: {len(self.mms_income_history)}")
+        print(f"Memory for sms: {len(self.sms_send_history) + len(self.sms_received_history)} / {self.max_sms}")
+        print(f"\tsms sent: {len(self.sms_send_history)}")
+        print(f"\tsms received: {len(self.sms_received_history)}")
+        print(f"Memory for mms: {len(self.mms_send_history) + len(self.mms_received_history)} / {self.max_mms_memo}")
+        print(f"\tmms sent: {len(self.mms_send_history)}")
+        print(f"\tmms received: {len(self.mms_received_history)}")
         print(f"Made calls:")
+        print(f"\tanswer: {len([call.react for call in self.call_send_history if call.react == 'answer'])}")
+        print(f"\tmissed: {len([call.react for call in self.call_send_history if call.react == 'missed'])}")
+        print(f"\treject: {len([call.react for call in self.call_send_history if call.react == 'reject'])}")
+        print(f"Received calls:")
         print(f"\tanswer: {len([call.react for call in self.call_received_history if call.react == 'answer'])}")
         print(f"\tmissed: {len([call.react for call in self.call_received_history if call.react == 'missed'])}")
         print(f"\treject: {len([call.react for call in self.call_received_history if call.react == 'reject'])}")
-        print(f"Income calls:")
-        print(f"\tanswer: {len([call.react for call in self.call_income_history if call.react == 'answer'])}")
-        print(f"\tmissed: {len([call.react for call in self.call_income_history if call.react == 'missed'])}")
-        print(f"\treject: {len([call.react for call in self.call_income_history if call.react == 'reject'])}")
         print(f"-" * 30)
 
     def send_sms(self, client, text):
         sms = Sms(self, client, text)
-        if self.checking_memory(self.max_sms, self.sms_received_history + self.sms_income_history):
-            self.sms_received_history.append(sms)
+        if self.checking_memory(self.max_sms, self.sms_send_history + self.sms_received_history):
+            self.sms_send_history.append(sms)
         self.operator.taking_action(sms)
 
     def send_mms(self, to, mms_size):
         mms = Mms(self, to, mms_size)
-        if self.checking_memory(self.max_mms_memo, self.mms_received_history + self.mms_income_history):
-            self.mms_received_history.append(mms)
+        if self.checking_memory(self.max_mms_memo, self.mms_send_history + self.mms_received_history):
+            self.mms_send_history.append(mms)
         self.operator.taking_action(mms)
 
     def calling(self, To):
         call = Call(self, To)
-        self.call_received_history.append(call)
+        self.call_send_history.append(call)
         self.operator.taking_action(call)
 
     @staticmethod
@@ -69,7 +71,7 @@ class Client:
                 item.react = "reject"
 
     @staticmethod
-    def checking_memory(max_memory, memory):
+    def checking_memory(max_memory, memory) -> bool:
         if max_memory > len(memory):
             return True
         else:
@@ -78,14 +80,50 @@ class Client:
     def getting_item(self, item):
         self.callin_action(item)
         if item.msg_type == "sms":
-            if self.checking_memory(self.max_sms, self.sms_received_history + self.sms_income_history):
-                self.sms_income_history.append(item)
+            if self.checking_memory(self.max_sms, self.sms_send_history + self.sms_received_history):
+                self.sms_received_history.append(item)
             else:
                 return False
         elif item.msg_type == "mms":
-            if self.checking_memory(self.max_mms_memo, self.mms_received_history + self.mms_income_history):
-                self.mms_income_history.append(item)
+            if self.checking_memory(self.max_mms_memo, self.mms_send_history + self.mms_received_history):
+                self.mms_received_history.append(item)
             else:
                 return False
         else:
-            self.call_income_history.append(item)
+            self.call_received_history.append(item)
+
+    @staticmethod
+    def make_sattlement_for_user(item: object.__class__, user):
+        if not isdir("Billing"):
+            os.mkdir('Billing')
+        if user.nr_tel == item.from_who.nr_tel:
+            if not isfile(f'Billing/{user.nr_tel}'):
+                with open(f'Billing/{user.nr_tel}', 'w', encoding="utf8") as outfile:
+                    description_list = ['sender_phonenumber',
+                                        'recipient_phonenumber',
+                                        'send_date',
+                                        'send_time',
+                                        'msg_type',
+                                        'msg_text',
+                                        'msg_size']
+                    for element in description_list:
+                        if element is description_list[len(description_list) - 1]:
+                            outfile.write(element + '\n')
+                        else:
+                            outfile.write(element)
+                            outfile.write(",")
+            with open(f'Billing/{user.nr_tel}', 'a', encoding="utf8") as csvfile:
+                csvfile_info_list = [item.from_who.nr_tel,
+                                     item.msg_recipient.nr_tel,
+                                     item.msg_send_date,
+                                     item.msg_send_time,
+                                     item.msg_type,
+                                     item.msg_text,
+                                     item.msg_size]
+                for info in csvfile_info_list:
+                    if info is csvfile_info_list[len(csvfile_info_list) - 1]:
+                        csvfile.write(str(info))
+                        csvfile.write('\n')
+                    else:
+                        csvfile.write(str(info))
+                        csvfile.write(",")
